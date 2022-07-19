5b051a49337c993e670baf319934688128dcc7ac
--replace-all <terrencejung928@gmail.com>
2022-07-18 20:40:32 -0700

    commit tree 31105a990228fb2fd6103b6536fad30952531937

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,8 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 end
